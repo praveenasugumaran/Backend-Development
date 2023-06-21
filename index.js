b2b7b740ab972f2c8a1af48c65cac293bd0e21c5
Pratik K <66823602+pratikiran@users.noreply.github.com>
2023-06-22 01:25:12 +0530

    Refactor token validation and middleware.
    Refactor code for improved token validation, add environment variable for JWT secret, and streamline middleware and error handling in Express server.

--- index.js
+++ index.js
@@ -1,87 +1,123 @@
-const express=require('express');
-const bodyParser=require('body-parser')
-const mongoose=require('mongoose')
-const bcrypt=require('bcrypt')
-const cookieParser=require('cookie-parser')
-const { createToken } =require('./JWT');
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.json())
-app.use(cookieParser())
-mongoose.connect("mongodb://127.0.0.1:27017/broadway")
-const usersSchema={
-    username:String,
-    email:String,
-    password:String
-}
-const users=mongoose.model("users",usersSchema)
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const { createToken } = require('./JWT');
+const rateLimit = require("express-rate-limit");
+const helmet = require('helmet');
+const cors = require('cors');
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/login.html")
-})
-app.post("/",async function(req,res){
-    const email=req.body.email;
-    const password=req.body.password;
-    try{
-        const userExists=await users.findOne({email})
-        const isPasswordMatch = await bcrypt.compare(password, userExists.password);
+const app = express();
 
-        if(userExists){
-            if(isPasswordMatch){
-                const accessToken=createToken(userExists);
-                res.cookie("access-token",accessToken,{
-                    maxAge:60*60*24*30*1000,
-                    httpOnly:true,
+// Middleware
+app.use(helmet()); // Security middleware
+app.use(cors()); // Handling CORS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(rateLimit({ // Rate limiting
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// Define User Schema
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// Create User Model
+const User = mongoose.model("User", userSchema);
+
+// Routes
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/login.html");
+});
+
+app.post("/", async (req, res) => {
+    // Input validation (example)
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+    try {
+        const userExists = await User.findOne({ email: req.body.email });
+        if (userExists) {
+            const isPasswordMatch = await bcrypt.compare(req.body.password, userExists.password);
+            if (isPasswordMatch) {
+                const accessToken = createToken(userExists, process.env.SECRET_KEY);
+                res.cookie("access-token", accessToken, {
+                    maxAge: 60 * 60 * 24 * 30 * 1000,
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'strict'
                 });
-                console.log(accessToken)
-                return res.status(200).json({message:'user successfully logged in'});
+                return res.status(200).json({ message: 'User successfully logged in' });
+            } else {
+                return res.status(400).json({ message: 'Invalid password' });
             }
-            else{
-                return res.status(400).json({message:'invalid password'})
-            }
-
+        } else {
+            return res.status(400).json({ message: 'User does not exist' });
         }
-        else{
-            return res.status(400).json({message:'user doesnt exists'})
-        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'An error occurred' });
     }
-    catch(error){
-        return res.status(500).json({message:'An error occurred'})
-    }
-})
+});
 
-app.get("/signup",function(req,res){
-    res.sendFile(__dirname+"/signup.html")
-})
-app.post("/signup",async function(req,res){
-    const username=req.body.username;
-    const email=req.body.emailid;
-    const TextPassword=req.body.password;
-    const hashedPassword=await bcrypt.hash(TextPassword,10)
-    try{
-        const existingUser=await users.findOne({email});
-        if(existingUser){
-            return res.status(400).json({message:'user already exists'});
-        }
-        else{
-            const newUser=new users({
-                username:username,
-                email:email,
-                password:hashedPassword
+app.get("/signup", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/signup", async (req, res) => {
+    // Input validation (example)
+    if (!req.body.username || !req.body.email || !req.body.password) {
+        return res.status(400).json({ message: 'Username, email, and password are required' });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        } else {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            const newUser = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: hashedPassword
             });
             await newUser.save();
-            return res.status(200).json({message:'user saved successfully'})
+            return res.status(200).json({ message: 'User saved successfully' });
         }
-    }
-    catch(error){
+    } catch (error) {
         console.error(error);
-        return res.status(500).json({message:'An error occurred'});
+        return res.status(500).json({ message: 'An error occurred' });
     }
-   
+});
 
-})
-app.listen(3000,function(req,res){
-    console.log("successfully listening to port 3000....")
-})
-//map api key : AIzaSyDOtqbkt7T3XhUtM-ABH8Hx9xj9aPOn9E8
-//map api reference: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Successfully listening on port ${port}...`);
+});
