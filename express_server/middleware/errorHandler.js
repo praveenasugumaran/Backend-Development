670869d2c49e776480c2de268c3ae621688a3423
Pratik K <undopatika@gmail.com>
2023-06-26 05:50:35 +0530

    Added comments for error handler satck

--- express_server/middleware/errorHandler.js
+++ express_server/middleware/errorHandler.js
@@ -1,7 +1,16 @@
+// Export a middleware function that handles errors
 module.exports = (err, req, res, next) => {
+    // Log the stack trace of the error to the console
     console.error(err.stack);
+
+    // Send a response with status 500 (Internal Server Error) 
+    // and a JSON object containing a message and the error
     res.status(500).json({
+        // A generic error message for the client
         message: 'An unexpected error occurred.',
+
+        // Include the error message from the caught error. 
+        // The toString() method is used to convert the error object to a string.
         error: err.toString()
     });
 };
